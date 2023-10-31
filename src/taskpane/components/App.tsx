ed6aed6797491a42c060880cf829e9c6ad2e5487
Michael Humann <Kingpin0509@gmail.com>
2023-10-31 19:11:23 +0100

    31102023

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 import HeroList, { HeroListItem } from "./HeroList";
 import Progress from "./Progress";
 import { NavCustomLinkExample } from "./Navigation";
-import { Nav, INavStyles, INavLinkGroup } from "@fluentui/react/lib/Nav";
+import { Nav, INavStyles, INavLinkGroup, INavLink } from "@fluentui/react/lib/Nav";
 /* global console, Excel, require  */
 
 export interface AppProps {
@@ -15,6 +15,98 @@ export interface AppProps {
 export interface AppState {
   listItems: HeroListItem[];
 }
+
+/* nav   */
+
+const navStyles: Partial<INavStyles> = {
+  root: {
+    width: 208,
+    height: 350,
+    boxSizing: "border-box",
+    border: "1px solid #eee",
+    overflowY: "auto",
+  },
+};
+
+const navLinkGroups: INavLinkGroup[] = [
+  {
+    links: [
+      {
+        name: "Home",
+        url: "http://example.com",
+        expandAriaLabel: "Expand Home section",
+        links: [
+          {
+            name: "Activity",
+            url: "http://msn.com",
+            key: "key1",
+            target: "_blank",
+          },
+          {
+            name: "MSN",
+            url: "http://msn.com",
+            disabled: true,
+            key: "key2",
+            target: "_blank",
+          },
+        ],
+        isExpanded: true,
+      },
+      {
+        name: "Documents",
+        url: "http://example.com",
+        key: "key3",
+        isExpanded: true,
+        target: "_blank",
+      },
+      {
+        name: "Pages",
+        url: "http://msn.com",
+        key: "key4",
+        target: "_blank",
+      },
+      {
+        name: "Notebook",
+        url: "http://msn.com",
+        key: "key5",
+        disabled: true,
+      },
+      {
+        name: "Communication and Media",
+        url: "http://msn.com",
+        key: "key6",
+        target: "_blank",
+      },
+      {
+        name: "News",
+        url: "http://cnn.com",
+        icon: "News",
+        key: "key7",
+        target: "_blank",
+      },
+    ],
+  },
+];
+
+export const NavBasicExample: React.FunctionComponent = () => {
+  return (
+    <Nav
+      onLinkClick={_onLinkClick}
+      selectedKey="key3"
+      ariaLabel="Nav basic example"
+      styles={navStyles}
+      groups={navLinkGroups}
+    />
+  );
+};
+
+function _onLinkClick(ev?: React.MouseEvent<HTMLElement>, item?: INavLink) {
+  if (item && item.name === "News") {
+    alert("News link clicked");
+  }
+}
+
+/* nav   */
 
 export default class App extends React.Component<AppProps, AppState> {
   constructor(props, context) {
@@ -205,9 +297,10 @@ export default class App extends React.Component<AppProps, AppState> {
         />
       );
     }
-    <NavCustomLinkExample></NavCustomLinkExample>
+    <NavCustomLinkExample></NavCustomLinkExample>;
     return (
       <div className="ms-welcome">
+        <NavBasicExample></NavBasicExample>
         <Header
           logo={require("./../../../assets/logo-filled.png")}
           title={this.props.title}
